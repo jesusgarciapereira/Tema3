f6d705f89ac28647bd9e56689d9ff0e129021f9e
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-17 14:42:34 +0100

    correcto sin comentar

--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		int n;
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Eco... n veces");
 		System.out.println("--------------");
 		System.out.println("Introduca el número de veces que se producirá el eco");
-		Funciones.eco(sc.nextInt());
+		n = sc.nextInt();
+		
+		Funciones.eco(n);
 		sc.close();
 	}
 

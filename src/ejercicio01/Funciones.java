69dcc5e4d16b921018a7b77d08d9e63139f27920
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-17 14:39:57 +0100

    Ejercicio01 sin comentar

--- src/ejercicio01/Funciones.java
+++ src/ejercicio01/Funciones.java
@@ -2,4 +2,10 @@ package ejercicio01;
 
 public class Funciones {
 
+	static void eco(int n) {
+		for (int i = 1; i <= n; i++) {
+			System.out.print("Eco...");
+		}
+		System.out.println();
+	}
 }
--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -1,9 +1,18 @@
 package ejercicio01;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Eco... n veces");
+		System.out.println("--------------");
+		System.out.println("Introduca el número de veces que se producirá el eco");
+		Funciones.eco(sc.nextInt());
+		sc.close();
 	}
 
 }

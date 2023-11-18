a17a572a9125592e9c5daa9b6f9c14b4f36410fc
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-18 12:42:13 +0100

    01 Hecho y comentado

--- src/ejercicio01/Main.java
+++ src/ejercicio01/Main.java
@@ -1,21 +1,44 @@
 package ejercicio01;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int n;
 
+		// Declaramos la variable que le pediremos al usuario
+		int n = -1;
+
+		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
 
+		// Presentamos el programa
 		System.out.println("Eco... n veces");
 		System.out.println("--------------");
-		System.out.println("Introduca el número de veces que se producirá el eco");
-		n = sc.nextInt();
+
+		// Bucle do-while para solicitar la entrada del usuario hasta que sea válida
+		do {
+			try {
+				/* Solicitamos al usuario el número de veces que mostrará el mensaje y leemos el
+				 * número */
+				System.out.println("Introduzca el número de veces que se producirá el eco (a partir de 0)");
+				n = sc.nextInt();
 		
+				/* Si el usuario ingresa un dato del tipo incorrecto, mostramos un mensaje de
+				 * error y limpiamos el buffer */ 
+			} catch (InputMismatchException e) {
+				System.out.println("El dato no es del tipo correcto");
+				sc.nextLine();
+			}
+
+			// Mientras el número sea menor que 0, repetiremos las instrucciones
+		} while (n < 0);
+
+		// Llamamos a la función eco() con el número introducido por el usuario como parámetro
 		Funciones.eco(n);
+
+		// Cerramos el Scanner
 		sc.close();
 	}
-
 }

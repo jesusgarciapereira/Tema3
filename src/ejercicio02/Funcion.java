e44dfa83fe34a61c653db1293f9a5eed4bbc77ca
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-19 21:57:54 +0100

    Estructura de los comentarios

--- src/ejercicio02/Funcion.java
+++ src/ejercicio02/Funcion.java
@@ -11,13 +11,14 @@ public class Funcion {
 		// Si los parámetros son iguales, mostrará este mensaje
 		if (a == b)
 			System.out.println("Nada, ambos son iguales");
-
-		else if (a < b) { // Si el primer parámetro es menor que el segundo
+		// Si el primer parámetro es menor que el segundo
+		else if (a < b) { 
 			// Bucle for para mostrar los números comprendidos entre ambos
 			for (int i = a + 1; i < b; i++) {
 				System.out.print(i + " ");
 			}
-		} else {// Si el primer parámetro es mayor que el segundo
+			// Si el primer parámetro es mayor que el segundo
+		} else {
 			// Bucle for para mostrar los números comprendidos entre ambos
 			for (int i = a - 1; i > b; i--) {
 				System.out.print(i + " ");

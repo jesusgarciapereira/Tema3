f36954af835aece34a7856a9b03e5e3d04cee4dd
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-19 18:52:15 +0100

    Ejercicio 5 Hecho y comentado

---
+++ src/ejercicio05/Funcion.java
@@ -0,0 +1,20 @@
+package ejercicio05;
+
+public class Funcion {
+
+	// Función tablaDel()
+	static void tablaDel(int n) {
+
+		// Presentamos la función
+		System.out.println("Tabla del " + n);
+		System.out.println();
+
+		/*
+		 * Bucle for para mostrar la multiplicación de n por todos los números del 1 al
+		 * 10
+		 */
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n + " x " + i + " = " + (n * i));
+		}
+	}
+}
---
+++ src/ejercicio05/Main.java
@@ -0,0 +1,49 @@
+package ejercicio05;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Declaramos la variable del número que le pediremos al usuario
+		int n = 0;
+
+		// Activamos el Scanner
+		Scanner sc = new Scanner(System.in);
+
+		// Presentamos el programa
+		System.out.println("Tabla del n");
+		System.out.println("-----------");
+
+		// Bucle do-while para solicitar la entrada del usuario hasta que sea válida
+		do {
+			try {
+				/*
+				 * Solicitamos al usuario el número de veces que mostrará el mensaje y leemos el
+				 * número
+				 */
+				System.out.println("Introduzca un valor para n (a partir de 1)");
+				n = sc.nextInt();
+
+				/*
+				 * Si el usuario ingresa un dato del tipo incorrecto, mostramos un mensaje de
+				 * error y limpiamos el buffer
+				 */
+			} catch (InputMismatchException e) {
+				System.out.println("El dato no es del tipo correcto");
+				sc.nextLine();
+			}
+
+			// Mientras el número sea menor que 1, repetiremos las instrucciones
+		} while (n <= 1);
+
+		// Llamamos a la función tablaDel() con el número introducido por el usuario como parámetro
+		Funcion.tablaDel(n);
+		
+		//Cerramos el Scanner
+		sc.close();
+	}
+
+}

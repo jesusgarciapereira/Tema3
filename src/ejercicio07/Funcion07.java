3025f37224452de2995c8936c8cf50f3c9d14cd2
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-20 18:52:04 +0100

    Comentarios mejorados

--- src/ejercicio07/Funcion07.java
+++ src/ejercicio07/Funcion07.java
@@ -8,10 +8,13 @@ public class Funcion07 {
 		// Si el número es 1, devolverá falso
 		if (n == 1)
 			return false;
+		
 		// Si el número es distinto de 1
 		else {
+			
 			// Bucle for desde el 2 hasta la raíz cuadrada del parámetro
 			for (int i = 2; i <= Math.sqrt(n); i++) {
+				
 				// Si, al menos, uno ellos es divisor del número, devolverá falso
 				if (n % i == 0)
 					return false;

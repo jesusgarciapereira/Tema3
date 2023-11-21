5fd9798336c6976e47109f845890758791994157
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-21 23:20:12 +0100

    Hecho lo básico

--- src/ejercicio10/Funcion10.java
+++ src/ejercicio10/Funcion10.java
@@ -2,4 +2,10 @@ package ejercicio10;
 
 public class Funcion10 {
 
+	static boolean fechaCorrecta(int dia, int mes, int annio) {
+		if (dia < 1 || dia > 30 || mes < 1 || mes > 12 || annio < 0)
+			return false;
+		else
+			return true;
+	}
 }
--- src/ejercicio10/Main10.java
+++ src/ejercicio10/Main10.java
@@ -1,10 +1,27 @@
 package ejercicio10;
 
+import java.util.Scanner;
+
 public class Main10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int dia, mes, annio;
+		boolean fechaCorrecta;
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Fecha correcta");
+		System.out.println("--------------");
+
+		dia = sc.nextInt();
+		mes = sc.nextInt();
+		annio = sc.nextInt();
+
+		fechaCorrecta = Funcion10.fechaCorrecta(dia, mes, annio);
+
+		System.out.println(
+				"La fecha " + dia + "/" + mes + "/" + annio + (fechaCorrecta ? " es correcta" : " no es correcta"));
+		sc.close();
 	}
 
 }

01833e1e3b6a0aaaca68944e59903abf451b627c
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-22 10:33:43 +0100

    Mejorado con un Switch

--- src/ejercicio01/Funcion01.java
+++ src/ejercicio01/Funcion01.java
@@ -4,16 +4,18 @@ public class Funcion01 {
 
 	// Función eco()
 	static void eco(int n) {
-		
+
+		// Switch para evaluar cada caso
+		switch (n) {
 		// Si el parámetro es igual a 0, mostrará un mensaje personalizado
-		if (n == 0)
-			System.out.println(n + " veces... silencio absoluto");
-		
-		// En caso contrario, mostrará n veces el mensaje "Eco..."
-		else {
+		case 0 -> System.out.println(n + " veces... silencio absoluto");
+		// En cualquier otro caso, mostrará n veces el mensaje "Eco..."
+		default -> {
 			for (int i = 1; i <= n; i++) {
 				System.out.print("Eco...");
 			}
 		}
+		}
+
 	}
 }

af03b119c066f956b83c6402529b08ac05379c6a
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-22 09:13:58 +0100

    Creacion y return de una variable y ternario

--- src/ejercicio03/Funcion03.java
+++ src/ejercicio03/Funcion03.java
@@ -4,15 +4,10 @@ public class Funcion03 {
 
 	// Función numeroMaximo()
 	static int numeroMaximo(int a, int b) {
+		int numeroMaximo;
 		
-		// Si el primer parámetro es mayor o igual que el segundo
-		if (a >= b)
-			// Nos devolverá el valor del primero
-			return a;
+		numeroMaximo = (a >= b) ? a : b;
 		
-		// Si el primer parámetro es menor que el segundo
-		else
-			// Nos devolverá el valor del segundo
-			return b;
+		return numeroMaximo;
 	}
 }

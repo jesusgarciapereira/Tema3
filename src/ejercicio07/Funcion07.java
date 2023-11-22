b1ee8a799f2c57459922f995f73d4c7811d0ce0d
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-22 09:15:46 +0100

    Declaracion y return de una variable

--- src/ejercicio07/Funcion07.java
+++ src/ejercicio07/Funcion07.java
@@ -4,24 +4,24 @@ public class Funcion07 {
 
 	// Función esPrimo()
 	public static boolean esPrimo(int n) {
-		
+
+		boolean esPrimo;
+
 		// Si el número es 1, devolverá falso
 		if (n == 1)
-			return false;
-		
+			esPrimo = false;
+
 		// Si el número es distinto de 1
 		else {
-			
 			// Bucle for desde el 2 hasta la raíz cuadrada del parámetro
 			for (int i = 2; i <= Math.sqrt(n); i++) {
-				
 				// Si, al menos, uno ellos es divisor del número, devolverá falso
 				if (n % i == 0)
-					return false;
+					esPrimo = false;
 			}
-			
 			// Si no encuentra ningún divisor, devolverá verdadero
-			return true;
+			esPrimo = true;
 		}
+		return esPrimo;
 	}
 }

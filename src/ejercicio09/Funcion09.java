2255379fc7d070704e7483de3a5738cedf7b1f1a
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-22 12:50:27 +0100

    Formato de comentarios

--- src/ejercicio09/Funcion09.java
+++ src/ejercicio09/Funcion09.java
@@ -4,8 +4,11 @@ public class Funcion09 {
 
 	// Función calculadora()
 	static double calculadora(double operandoA, double operandoB, int operacion) {
+		
+		// Declaramos la variable que devolverá la función
 		double resultado;
-		// Switch para devolver el resultado de la operación
+		
+		// Switch para asignar un valor al resultado, según cada caso
 		resultado = switch (operacion) {
 		case 1 -> operandoA + operandoB;
 		case 2 -> operandoA - operandoB;
@@ -13,6 +16,8 @@ public class Funcion09 {
 		case 4 -> operandoA / operandoB;
 		default -> 0;
 		};
+		
+		// Devolverá el valor del resultado
 		return resultado;
 
 	}

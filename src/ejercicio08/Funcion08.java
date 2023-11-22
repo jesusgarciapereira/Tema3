d82c98eaabcd0505ace32a664a8a0c78c5b262e0
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-22 09:16:12 +0100

    Creacion de variable y asignacion de una funcion

--- src/ejercicio08/Funcion08.java
+++ src/ejercicio08/Funcion08.java
@@ -10,14 +10,17 @@ public class Funcion08 {
 		// Declaramos la variable del contador de divisores primos
 		int contadorDivisoresPrimos = 0;
 		
+		boolean esPrimo;
+		
 		// Bucle for desde el 2 hasta la raíz cuadrada del parámetro
 		for (int i = 2; i <= Math.sqrt(n); i++) {
 			
+			esPrimo = Funcion07.esPrimo(i);
 			/*
 			 * Por cada uno de ellos que sea primo y divisor del parámetro, nuestro contador
 			 * se incrementará
 			 */
-			if (Funcion07.esPrimo(i) == true && n % i == 0)
+			if (esPrimo == true && n % i == 0)
 				contadorDivisoresPrimos++;
 		}
 		// Devolverá el valor de nuestro contador

1be3cb28a8a953d65b8130990690cbc905335adf
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-22 13:59:28 +0100

    10 en proceso

--- src/ejercicio10/Funcion10.java
+++ src/ejercicio10/Funcion10.java
@@ -2,12 +2,16 @@ package ejercicio10;
 
 public class Funcion10 {
 
+	// Función esFechaCorrecta()
 	static boolean esFechaCorrecta(int dia, int mes, int annio) {
 
+		// Declaramos la variable que devolverá la función
 		boolean fechaCorrecta;
 
-		fechaCorrecta = (dia > 0 && dia < 31 && mes > 0 && mes < 13) ? true : false;
+		// Asignamos el valor del boolean con un ternario
+		fechaCorrecta = dia > 0 && dia < 31 && mes > 0 && mes < 13 ? true : false;
 
+		// Devolverá el valor de nuestro boolean
 		return fechaCorrecta;
 	}
 }
--- src/ejercicio10/Main10.java
+++ src/ejercicio10/Main10.java
@@ -1,20 +1,57 @@
 package ejercicio10;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main10 {
 
 	public static void main(String[] args) {
 		int dia, mes, annio;
+
+		/*
+		 * Declaramos variables booleanas para verificar si los datos ingresados son del
+		 * tipo correcto
+		 */
+		boolean tipoCorrectoDia = false;
+		boolean tipoCorrectoMes;
+		boolean tipoCorrectoAnnio;
+
 		boolean fechaCorrecta;
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Fecha correcta");
 		System.out.println("--------------");
 
+		// Bucle do-while para solicitar la entrada del usuario hasta que sea válida
+		do {
+			try {
+				// Solicitamos al usuario un valor para la operacion y lo leemos
+				System.out.println("Introduzca el número correspondiente al día");
+				dia = sc.nextInt();
+
+				// Marcamos 'tipoCorrectoDia' como true si el valor de 'dia' es del tipo
+				// correcto
+				tipoCorrectoDia = true;
+
+				/*
+				 * Si el usuario ingresa un dato del tipo incorrecto, mostramos un mensaje de
+				 * error y limpiamos el buffer
+				 */
+			} catch (InputMismatchException e) {
+				System.out.println("El dato no es del tipo correcto");
+				sc.nextLine();
+			}
+
+			/*
+			 * Mientras la variable 'dia' no sea del tipo correcto, repetiremos las
+			 * instrucciones
+			 */
+		} while (!tipoCorrectoDia);
+
 		System.out.println("Introduzca el número correspondiente al día");
 		dia = sc.nextInt();
+
 		System.out.println("Introduzca el número correspondiente al mes");
 		mes = sc.nextInt();
 		System.out.println("Introduzca el número correspondiente al año");

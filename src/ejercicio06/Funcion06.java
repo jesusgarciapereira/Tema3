3c6f29facd3528a9458485f11cd210704823ee4d
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-22 09:15:10 +0100

    Mejorado con switch

--- src/ejercicio06/Funcion06.java
+++ src/ejercicio06/Funcion06.java
@@ -8,17 +8,19 @@ public class Funcion06 {
 		// Declaramos la constante del número pi
 		final double PI = Math.PI;
 
+		switch (opcion) {
 		// Si el primer parámetro es 1
-		if (opcion == 1) {
+		case 1 -> {
 			// Calculamos el área del cilindro y lo mostramos
 			double area = 2 * PI * radio * (altura + radio);
 			System.out.println("El área del cilindro es " + area + " cm2");
-			// Si el primer parámetro es 2
-		} else {
+		}
+		// Si el primer parámetro es distinto de 1
+		default -> {
 			// Calculamos el volumen del cilindro y lo mostramos
 			double volumen = PI * Math.pow(radio, 2) * altura;
 			System.out.println("El volumen del cilindro es " + volumen + " cm3");
 		}
-
+		}
 	}
 }

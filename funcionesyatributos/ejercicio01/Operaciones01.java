5cf976f5cac5493d2600b159fe4892e66131275c
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-12-01 14:45:10 +0100

    Practica mucho

--- funcionesyatributos/ejercicio01/Operaciones01.java
+++ funcionesyatributos/ejercicio01/Operaciones01.java
@@ -1,5 +1,41 @@
 package ejercicio01;
 
 public class Operaciones01 {
+	double num1, num2;
 
+	public Operaciones01() {
+	}
+
+	public Operaciones01(double num1, double num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+
+	public double suma() {
+		return num1 + num2;
+	}
+
+	public double resta() {
+		return num1 - num2;
+	}
+
+	public double multiplicacion() {
+		return num1 * num2;
+	}
+
+	public double division() {
+		return num1 / num2;
+	}
+
+	public double max() {
+		double numMax;
+		numMax = num1 >= num2 ? num1 : num2;
+		return numMax;
+	}
+
+	public double min() {
+		double numMin;
+		numMin = num1 <= num2 ? num1 : num2;
+		return numMin;
+	}
 }
--- funcionesyatributos/ejercicio01/Principal01.java
+++ funcionesyatributos/ejercicio01/Principal01.java
@@ -1,37 +1,44 @@
 package ejercicio01;
 
 import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Principal01 {
-	
+
 	static Scanner sc = new Scanner(System.in);
-	
 
 	public static void main(String[] args) {
+		int opcion = -1;
+		double numA;
+		double numB;
+		double resultado;
 
-		Principal01.menu();
-		sc.close();
-	}
+		opcion = Principal01.menu();
+		while (opcion != 0) {
+			
+			
+				numA = Principal01.pideNumero();
+				numB = Principal01.pideNumero();
 
-	public static int pideOpcionCorrecta() {
-		int opcionCorrecta = -1;
+				Operaciones01 operacion = new Operaciones01(numA, numB);
 
-		do {
-			try {
-				System.out.println("Elija una de estas opciones");
-				opcionCorrecta = sc.nextInt();
-				sc.nextLine();
-				
+				resultado = switch (opcion) {
+				case 1 -> operacion.suma();
+				case 2 -> operacion.resta();
+				case 3 -> operacion.multiplicacion();
+				case 4 -> operacion.division();
+				case 5 -> operacion.max();
+				case 6 -> operacion.min();
+				default -> Math.sqrt(-1);
+				};
 
-			} catch (InputMismatchException e) {
-				System.out.println("El dato no es del tipo correcto, vuelva a intentarlo");
-				sc.nextLine();
-			} 
-		} while (opcionCorrecta < 0 || opcionCorrecta > 6);
+				System.out.println(resultado);
 		
-
-		return opcionCorrecta;
+			opcion = Principal01.menu();
+		}
+		System.out.println("Saliendo del programa");
+		sc.close();
 	}
 
 	public static int menu() {
@@ -48,11 +55,48 @@ public class Principal01 {
 		System.out.println("0. Salir");
 		System.out.println();
 
-		
 		opcion = Principal01.pideOpcionCorrecta();
 
 		return opcion;
 
 	}
 
+	public static double pideNumero() {
+		double numDouble = 0;
+		boolean numDoubleCorrecto = false;
+
+		sc.useLocale(Locale.US);
+
+		do {
+			try {
+				System.out.println("Elija un número para la operacion (si tiene decimales use punto)");
+				numDouble = sc.nextDouble();
+				numDoubleCorrecto = true;
+			} catch (InputMismatchException e) {
+				System.out.println("El dato no es del tipo correcto, vuelva a intentarlo");
+			} finally {
+				sc.nextLine();
+			}
+		} while (!numDoubleCorrecto);
+
+		return numDouble;
+	}
+
+	// FUNCION PROPIA
+	public static int pideOpcionCorrecta() {
+		int opcionCorrecta = -1;
+
+		do {
+			try {
+				System.out.println("Elija una de estas opciones");
+				opcionCorrecta = sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("El dato no es del tipo correcto, vuelva a intentarlo");
+			} finally {
+				sc.nextLine();
+			}
+		} while (opcionCorrecta < 0 || opcionCorrecta > 6);
+
+		return opcionCorrecta;
+	}
 }
